16969adab5e23d1b3125a77393a67144133d9e31
renebianco <rene.weiss@embarc.de>
2019-09-23 19:43:32 +0200

    First Commit Scientist Sample

---
+++ Basta2019_Scientist/Basta2019_Scientist.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Basta2019_Scientist.ComplicatedCalculation;
+using Basta2019_Scientist.ComplicatedCalculation.v2;
+using GitHub;
+
+namespace Basta2019_Scientist.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IResultPublisher publisher = new MyResultPublisher();
+            Scientist.ResultPublisher = publisher;
+
+            for (int i = 0; i < 5; i++)
+            {
+                int randomInput = new Random().Next(4);
+
+                Console.WriteLine($"Random num is: {randomInput}");
+
+                int result = Scientist.Science<int>("First experiment", experiment =>
+                {
+                    experiment.AddContext("Random Data", randomInput);
+
+                    experiment.Use(() => new ComplicatedCalc_V1().GetResult(randomInput)); // Old Version
+                    experiment.Try(() => new ComplicatedCalc_V2().GetResult(randomInput)); // New Version
+                });
+                Console.WriteLine($"Result: {result}"); // Result is always Old Version
+            }
+
+        }
+    }
+}
---
+++ Basta2019_Scientist/Basta2019_Scientist.ComplicatedCalculation/ComplicatedCalc_V1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace Basta2019_Scientist.ComplicatedCalculation
+{
+    public class ComplicatedCalc_V1
+    {
+        public ComplicatedCalc_V1()
+        {
+        }
+
+        public int GetResult(int context)
+        {
+            if (context == 1)
+            {
+                int result;
+
+                // Do other complicated stuff
+                Thread.Sleep(20);
+
+                int sum1 = context;
+                int sum2 = context;
+
+                result = sum1 + sum2;
+
+                return result;
+            }
+
+            if (context == 2)
+            {
+                int result;
+
+                // Do other complicated stuff
+                Thread.Sleep(30);
+
+                int mult1 = context;
+                int mult2 = context;
+
+                result = mult1 * mult2;
+
+                return result;
+            }
+
+            if (context == 3)
+            {
+                int result;
+
+                // Do other complicated stuff
+                Thread.Sleep(40);
+
+                int sum1 = context;
+                int sum2;
+                int mult1 = context;
+                int mult2 = context;
+
+                sum2 = mult1 * mult2;
+
+                result = sum1 + sum2;
+
+                return result;
+            }
+
+            // Do other complicated stuff
+            Thread.Sleep(10);
+            return context;
+        }
+    }
+}
